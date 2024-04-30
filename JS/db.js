c766bc0e5702cca578ca59055805f652f50f69e2
B.Silva <138171223+3run0Silva@users.noreply.github.com>
2024-04-30 13:40:00 +0200

    feat: added firebase + del: removed test files

---
+++ JS/db.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA1IZQpJ1mK5JJAfhwEYCwrTUxa9gH5MAQ",
+  authDomain: "tdl-app-bd8ed.firebaseapp.com",
+  databaseURL: "https://tdl-app-bd8ed-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "tdl-app-bd8ed",
+  storageBucket: "tdl-app-bd8ed.appspot.com",
+  messagingSenderId: "584421172141",
+  appId: "1:584421172141:web:4175bb718132e176acb724"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
